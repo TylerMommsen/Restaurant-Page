300898728b825f3a09c55149865a285707958eac
Tyler Mommsen <65496518+TylerMommsen@users.noreply.github.com>
2023-08-10 21:24:50 +0300

    Updated readme and update webpack config for production.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Restaurant-Page
+
+Basic restaurant site created with HTML, CSS and JS  
+
+[Live Demo](https://tylermommsen.github.io/Restaurant-Page/) 👈
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,10 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: {
         index: './src/index.js',
     },
-    devtool: 'inline-source-map',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
